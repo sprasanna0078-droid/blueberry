d301d18c6760fee74497c16f7e338990e0f39b77
sprasanna0078-droid <sprasanna0078@gmail.com>
2026-04-01 11:22:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Blueberry's The Vintage Restaurant Website Loaded");
+
+// Simple alert when page loads
+window.onload = function() {
+  alert("Welcome to Blueberry's The Vintage Restaurant!");
+};
